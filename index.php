1392aea58ed813f156445b841f1be933ccb39199
Mauro Miranda <solluzitecnologia@gmail.com>
2019-10-24 13:46:26 -0300

    Primeiro Commit

--- index.php
+++ index.php
@@ -9,13 +9,8 @@ declare(strict_types=1);
  * @copyright 2018 Solluzi Soluções Integradas
  */
 header('Access-Control-Allow-Credentials:true');
-<<<<<<< HEAD
 header("Access-Control-Allow-Origin: ".getenv('APP_URL'));
 header("Access-Control-Allow-Headers: token, Content-Type");
-=======
-header("Access-Control-Allow-Origin: http://localhost:8000");
-//header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
->>>>>>> b90c120a6682d1886c49803ad92ddd4da1bf69a0
 header("Access-Control-Expose-Headers: Content-Length, X-JSON");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 
@@ -50,29 +45,6 @@ $router = new AltoRouter();
 $router->setBasePath('/v1');
 require_once 'config/routes/app/routes.php';
 
-<<<<<<< HEAD
-
-
-########################## Rotas APP ######################################
-require_once 'config/routes/app/auth.php';       // Login
-require_once 'config/routes/app/home.php';       // Home
-require_once 'config/routes/app/usuario.php';    // User
-require_once 'config/routes/app/empresa.php';    // Empresa
-require_once 'config/routes/app/conexaoerp.php'; // Permission
-require_once 'config/routes/app/conexaomsg.php'; // Permission
-require_once 'config/routes/app/grupo.php'; // Grupo
-
-
-########################## Rotas Estoque ##################################
-#require_once 'config/routes/estoque/departamento.php';  //departamento
-#require_once 'config/routes/estoque/grupo.php';         //grupo
-#require_once 'config/routes/estoque/unidade.php';       //unidade
-#require_once 'config/routes/estoque/tipomovimento.php'; //tipomovimento
-#require_once 'config/routes/estoque/localestoque.php';  //localestoque
-#require_once 'config/routes/estoque/produto.php';       //produto
-#require_once 'config/routes/estoque/estorno.php';       //estorno
-#require_once 'config/routes/estoque/historico.php';       //historico*/
-=======
 ########################## Rotas APP ######################################
 /*require_once 'config/routes/app/signup.php';
 require_once 'config/routes/app/auth.php';       // Login
@@ -91,7 +63,6 @@ require_once 'config/routes/estoque/localestoque.php';  //localestoque
 require_once 'config/routes/estoque/produto.php';       //produto
 require_once 'config/routes/estoque/estorno.php';       //estorno
 require_once 'config/routes/estoque/historico.php';       //historico*/
->>>>>>> b90c120a6682d1886c49803ad92ddd4da1bf69a0
 
 
 ######################### Validação as Rotas ##############################
